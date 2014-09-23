b0a0e91b53620249a475a2bf9f5f410535eece19
thib3113 <thib3113@gmail.com>
2014-09-23 08:45:53 +0200

    passage en béta 2

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+	define('PROGRAM_NAME','EVA');
+	define('PROGRAM_AUTHOR','Thibaut SEVERAC');
+	define('PROGRAM_VERSION','0.2');
+    define('DB_TYPE','SQLITE');//SQLITE ou MYSQL
+    define('DB_PREFIX','EVA_');
+	define('DB_NAME','/db/.database.db');
+	define('LOG_FILE','/log/log.txt');
+	define('DEBUG', 1);
+    define('SMARTY_SPL_AUTOLOAD',false);
+    define('SMARTY_DIR','classes/');
