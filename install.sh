d53312c4a163d712f42f2e1fe627bd0ce0c05ec9
thib3113 <thib3113@gmail.com>
2014-12-11 01:06:39 +0100

    avancée install auto

--- install.sh
+++ install.sh
@@ -1,118 +1,246 @@
 #! /bin/bash
 
+url_git=""
+log_folder="/var/log/eva"
+log_file="install.log"
+log_error_file="error.install.log"
+
 GREEN="\\033[1;32m"
 WHITE="\\033[0;39m"
 RED="\\033[1;31m"
 BLUE='\e[0;34m'
 PURPLE='\e[0;35m'
 
-affich_ok(){
-    echo -e " [${GREEN}FAIT${WHITE}]"
+affich(){
+    format=""
+    for var in "$@"
+    do
+        if [ $(echo "action ok error already point " | grep -- "$var\s" | wc -l) -eq 1 ]
+        then
+            format=$var
+        fi
+
+        if [ "$var" == "-no-log" ]
+        then
+            no_log=1
+        fi
+
+        text=$var
+    done
+
+    case $format in
+        "action")
+            echo -ne "${PURPLE}- ${text}${WHITE}"
+            if [ ! -z $no_log ]
+            then
+                echo -ne "${PURPLE}- ${text}${WHITE}" >> "$log_folder/$log_file"
+            fi
+        ;;
+        "ok")
+            echo -e "[${GREEN}FAIT${WHITE}]"
+            if [ ! -z $no_log ]
+            then
+                echo -e "[${GREEN}FAIT${WHITE}]" >> "$log_folder/$log_file"
+            fi
+        ;;
+        "error")
+            echo -e "[${RED}ERREUR${WHITE}]"
+            if [ ! -z $no_log ]
+            then
+                echo -e "[${RED}ERREUR${WHITE}]" >> "$log_folder/$log_file"
+            fi
+            exit 1
+        ;;
+        "already")
+            echo -e "[${BLUE}DEJA FAIT${WHITE}]"
+            if [ ! -z $no_log ]
+            then
+                echo -e "[${BLUE}DEJA FAIT${WHITE}]" >> "$log_folder/$log_file"
+            fi
+        ;;
+        "point")
+            echo -ne ". "
+            if [ ! -z $no_log ]
+            then
+                echo -ne ". " >> "$log_folder/$log_file"
+            fi
+        ;;
+    esac
 }
 
-affich_deja_fait(){
-    echo -e " [${BLUE}DEJA FAIT${WHITE}]"
+check_install(){
+    if [ -z $2 ] || [ $2 != "-r"]
+    then
+        soft="${1}\s"
+    else
+        soft=$1
+    fi
+    # echo "sudo dpkg --get-selections | grep \"$soft\" | grep -v deinstall"
+    if [ $(sudo dpkg --get-selections | grep "$soft" | grep -v deinstall | wc -l) -gt 0 ]
+    then
+        echo "installation fail " >> "$log_folder/$log_file"
+        echo "commande sudo dpkg --get-selections | grep \"$soft\" | grep -v deinstall" >> "$log_folder/$log_file"
+        sudo dpkg --get-selections | grep "$soft" | grep -v deinstall >> "$log_folder/$log_file"
+        return 1
+    else
+        return 0
+    fi
 }
 
-affich_erreur(){
-    echo -e " [${RED}ERREUR${WHITE}]"
+install(){
+    apt-get install -y $1 >> "$log_folder/$log_file" 2>> "$log_folder/$log_error_file"
+    if ! check_install $1;
+    then
+        affich error
+    fi
+
 }
 
-a_p(){
-    echo -ne ". "
-}
+if [ "$UID" -ne 0 ]
+then
+    echo -e "Le script doit être lancé en sudo [${RED}ERREUR${WHITE}]"
+    exit 0
+fi
 
-affich_action(){
-    echo -ne "${PURPLE}- ${1}${WHITE}"
-}
+GRN=$GREEN
 
-echo -e "${GREEN}   .~~.   .~~.   ${WHITE}             "
-echo -e "${GREEN}  '. \ ' ' / .'  ${WHITE}       "
-echo -e "${RED}   .~ .~~~..~.   ${WHITE}   _____ _   _  ___  "
-echo -e "${RED}  : .~.'~'.~. :  ${WHITE}  |  ___| | | |/ _ \ "
-echo -e "${RED} ~ (   ) (   ) ~ ${WHITE}  | |__ | | | / /_\ \\"
-echo -e "${RED}( : '~'.~.'~' : )${WHITE}  |  __|| | | |  _  |"
-echo -e "${RED} ~ .~ (   ) ~. ~ ${WHITE}  | |___\ \_/ / | | |"
-echo -e "${RED}  (  : '~' :  )  ${WHITE}  \____/ \___/\_| |_/"
-echo -e "${RED}   '~ .~~~. ~'   ${WHITE}                      "
-echo -e "${RED}       '~'       ${WHITE}                      "
+echo -e "${GRN}   .~~.   .~~.   ${WHITE}                                   "
+echo -e "${GRN}  '. \ ' ' / .'  ${WHITE}                                   "
+echo -e "${RED}   .~ .~~~..~.   ${WHITE}   ________  ____   ____    _      "
+echo -e "${RED}  : .~.'~'.~. :  ${WHITE}  |_   __  ||_  _| |_  _|  / \     "
+echo -e "${RED} ~ (   ) (   ) ~ ${WHITE}    | |_ \_|  \ \   / /   / _ \    "
+echo -e "${RED}( : '~'.~.'~' : )${WHITE}    |  _| _    \ \ / /   / ___ \   "
+echo -e "${RED} ~ .~ (   ) ~. ~ ${WHITE}   _| |__/ |    \ ' /  _/ /   \ \_ "
+echo -e "${RED}  (  : '~' :  )  ${WHITE}  |________|     \_/  |____| |____|"
+echo -e "${RED}   '~ .~~~. ~'   ${WHITE}                                   "
+echo -e "${RED}       '~'       ${WHITE}                                   "
 echo -e "Lien github : ${GREEN}https://github.com/thib3113/EVA${WHITE}"
 echo -e "Lien du site : ${GREEN}http://evaproject.net/${WHITE}"
 echo -e "En cas de problème, merci de joindre vos deux fichiers présent dans le dossier log avec votre problème"
-echo -e "${GREEN}Crée par SEVERAC Thibaut étudiant${WHITE}\n"
+echo -e "${GREEN}Crée par SEVERAC Thibaut - étudiant - ${PURPLE}http://cv.thib3113.fr${WHITE}\n"
 
-
-affich_action "Création d'un dossier pour les logs "
-if [ -d log ]
+affich action "Création d'un dossier pour les logs "
+if [ -d $log_folder ]
 then
-    a_p
-    a_p
-    a_p
-    affich_deja_fait
+        affich -no-log point
+        affich -no-log point
+        affich -no-log point
+    affich already
 else
-    a_p
-    mkdir log
-    a_p
-    a_p
-    affich_ok
+        affich -no-log point
+    mkdir -p $log_folder
+        affich -no-log point
+    date >> "$log_folder/$log_file"
+        affich -no-log point
+    affich -no-log ok
 fi
 
-affich_action "mise à jour de la liste des paquets "
-a_p
-sudo apt-get update >> log/install.log 2>> log/install.error.log
-a_p
-a_p
-affich_ok
 
-affich_action "Recherche d'un serveur web "
-if [ $(sudo dpkg --get-selections | grep apache | grep -v deinstall | wc -l) -gt 0 ]
+affich action "Déplacement dans /var/www "
+if [ -d /var/www ]
 then
-    webserver="apache"
+        affich point
+        affich point
+        cd /var/www
+        affich point
+    affich ok
+else
+        affich point
+    mkdir -p /var/www
+    echo -ne "création réussie "
+        affich point
+    cd /var/www
+        affich point
+    affich ok
 fi
 
-a_p
 
-if [ $(sudo dpkg --get-selections | grep nginx | grep -v deinstall | wc -l) -gt 0 ]
+affich action "Mise à jour de la liste des paquets "
+    affich point
+    affich point
+sudo apt-get update >> "$log_folder/$log_file" 2>> $log_error_file
+    affich point
+affich ok
+
+affich action "Installation de GIT "
+if check_install "git";
 then
-    webserver="nginx"
+        affich point
+        affich point
+        affich point
+    affich already
+else
+    install git
+        affich point
+        affich point
+        affich point
+    affich ok
 fi
 
-a_p
-
-if [ $(sudo dpkg --get-selections | grep lighttpd | grep -v deinstall | wc -l) -gt 0 ]
+affich action "Recherche d'un serveur web "
+if check_install apache2;
 then
-    webserver="lighttpd"
+    webserver="Apache"
 fi
 
-a_p
+    affich point
+
+
+if check_install nginx;
+then
+    webserver="Nginx"
+fi
+
+    affich point
+
+if check_install lighttpd;
+then
+    webserver="Lighttpd"
+fi
+
+    affich point
 
 if [ ! -z $webserver ]
 then
-    echo -ne " ${webserver} est déjà installé !"
-    affich_deja_fait
+    echo -ne "${webserver} est déjà installé ! "
+    affich already
 else
-    echo " : Aucun serveur web installé, lighttpd sera installé !"
-    affich_ok
+    echo -ne "Aucun serveur web installé ! "
+    affich ok
 fi
 
 if [ -z $webserver ]
 then
-    affich_action "installation de lighttpd"
-    sudo apt-get install -y lighttpd >> log/install.log 2>> log/install.error.log
-    affich_ok
+    affich action "Installation de lighttpd "
+    if install lighttpd ; then affich error ;fi
+        affich point
+        affich point
+        affich point
+    affich ok
 
-    affich_action "installation de PHP & SQLite"
-    sudo apt-get install -y php5 php5-cgi php5-sqlite sqlite >> log/install.log 2>> log/install.error.log
-    /etc/init.d/lighttpd force-reload >> log/install.log 2>> log/install.error.log
-    affich_ok
+    affich action "Installation de PHP & SQLite "
+    if install php5 ; then affich error ;fi
+        affich point
+    if install php5-cgi ; then affich error ;fi
+        affich point
+    if install php5-sqlite ; then affich error ;fi
+        affich point
+    if install sqlite ; then affich error ;fi
+        affich point
+    /etc/init.d/lighttpd force-reload >> "$log_folder/$log_file" 2>> $log_error_file
+        affich point
+    affich ok
 
-    affich_action "Configuration de lighttpd"
-    sudo lighttpd-enable-mod fastcgi-php >> log/install.log
-    sudo lighttpd-enable-mod cgi >> log/install.log
+    affich action "Configuration de lighttpd "
+    sudo lighttpd-enable-mod fastcgi-php >> "$log_folder/$log_file"
+        affich point
+    sudo lighttpd-enable-mod cgi >> "$log_folder/$log_file"
+        affich point
     echo -e '$HTTP["url"] =~ "/cgi-bin/" {\n\tcgi.assign = ( "" => "" )\n}' >> /etc/lighttpd/conf-enabled/10-cgi.conf
-    sudo /etc/init.d/lighttpd force-reload >> log/install.log
-    affich_ok
-else
-    affich_erreur
-    exit 1
+        affich point
+    sudo /etc/init.d/lighttpd force-reload >> "$log_folder/$log_file"
+        affich point
+    affich ok
 fi
+
+
